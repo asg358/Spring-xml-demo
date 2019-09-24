5cae2141de17a2f16109b928d4fc1bd9cfe1e496
asg358445 <abhinav.garia@cgi.com>
2019-09-24 17:27:07 +0530

    Task2

--- SpringTasksA/src/main/java/com/stackroute/Main.java
+++ SpringTasksA/src/main/java/com/stackroute/Main.java
@@ -11,18 +11,17 @@ public class Main {
 
     public static void main( String[] args )
     {
-
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
         Movie movie=context.getBean("movie",Movie.class);
         System.out.println("Name of the actor "+movie.getActor().getName());
         System.out.println("Gender is "+movie.getActor().getGender());
         System.out.println("Age is "+ movie.getActor().getAge());
 
-        BeanFactory beanFactory=new XmlBeanFactory(new ClassPathResource("beans.xml"));
-        Movie movie1=beanFactory.getBean("movie",Movie.class);
-        System.out.println("Actor's name is "+ movie1.getActor().getName());
-        System.out.println("Gender is "+movie1.getActor().getGender());
-        System.out.println("Age is "+ movie1.getActor().getAge());
-        
+        Movie movieA=context.getBean("movie",Movie.class);
+        System.out.println(movieA==movie);
+
+        Movie movieB=context.getBean("movieB",Movie.class);
+        Movie movieC=context.getBean("movieB",Movie.class);
+        System.out.println(movieC==movieB);
     }
 }
--- SpringTasksA/src/main/java/com/stackroute/domain/Movie.java
+++ SpringTasksA/src/main/java/com/stackroute/domain/Movie.java
@@ -2,13 +2,13 @@ package com.stackroute.domain;
 
 public class Movie {
 
-    Actor actor;
+    private Actor actor;
+
+    public Movie(Actor actor) {
+        this.actor = actor;
+    }
 
     public Actor getActor() {
         return actor;
-    }
-
-    public void setActor(Actor actor) {
-        this.actor = actor;
     }
 }
